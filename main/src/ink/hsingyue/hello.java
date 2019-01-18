2555b857ec30857bf475d6b6b864c12b5fc250cc
HsingYue <wo386293666@qq.com>
2019-01-18 22:35:01 +0800

    finished.

--- main/src/ink/hsingyue/hello.java
+++ main/src/ink/hsingyue/hello.java
@@ -1,9 +1,14 @@
 package ink.hsingyue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class hello {
 
     public static void main(String[] args) {
+
         System.out.println("Hello Git");
+
     }
 
 }
